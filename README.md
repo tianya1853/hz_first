edf5f6ca024b917fac6433b9318b64f821d3f8ed
root <root@kasuo-desktop.(none)>
2017-04-21 11:22:27 +0800

    increase project path

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hz_libc
 hz_libc for my first code in github,I'm so sorry for my poor english
+Modify shell to makefile as soon as possible
--- project/example/build.sh
+++ project/example/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+PATH_TOP=../..
 
 
 if [ $1'x' == 'cleanx' ];then
@@ -9,20 +9,20 @@ if [ $1'x' == 'cleanx' ];then
 fi
 	
 
-PATH_SQLITE=../sqlite/sqlite-snapshot-201703241759
+PATH_SQLITE=$PATH_TOP/sqlite/sqlite-snapshot-201703241759
 PATH_SQLITE_LIBS=$PATH_SQLITE/.libs
 CC="gcc"
-CFLAG="-Wall -g -lpthread -lrt -Ihz_libc -lsqlite3 -L$PATH_SQLITE_LIBS -I$PATH_SQLITE"
+CFLAG="-Wall -g -lpthread -lrt -I$PATH_TOP/hz_libc -lsqlite3 -L$PATH_SQLITE_LIBS -I$PATH_SQLITE"
 
-SRCS_SERVER="server.c hz_libc/*.c"
-SRCS_CLIENT="client.c hz_libc/*.c"
+SRCS_SERVER="server.c $PATH_TOP/hz_libc/*.c"
+SRCS_CLIENT="client.c $PATH_TOP/hz_libc/*.c"
 SRCS_TEST_SQLITE="test_sqlite.c  hz_libc/*.c"
 
 
 
 TARGET_SERVER="server"
 TARGET_CLIENT="client"
-TARGET_TEST_SQLITE="test_sqlite"
+#TARGET_TEST_SQLITE="test_sqlite"
 
 SERVER_BUILD="$CC $SRCS_SERVER $CFLAG -o $TARGET_SERVER "
 CLIENT_BUILD="$CC $SRCS_CLIENT $CFLAG -o $TARGET_CLIENT "
@@ -32,4 +32,4 @@ TEST_SQLITE_BUILD="$CC $SRCS_TEST_SQLITE $CFLAG -o $TARGET_TEST_SQLITE "
 
 $SERVER_BUILD
 $CLIENT_BUILD
-$TEST_SQLITE_BUILD
+#$TEST_SQLITE_BUILD
